09ea80495f176fb432eb7582a7dd83bc02a270e0
Juan <40309961+JuanGonzalez99@users.noreply.github.com>
2021-10-02 11:33:43 -0300

    busqueda por id y modificacion

---
+++ app/src/main/java/com/example/tp4_almacenamientoexterno/dao/BusquedaArticulo.java
@@ -0,0 +1,46 @@
+package com.example.tp4_almacenamientoexterno.dao;
+
+import android.os.AsyncTask;
+
+import com.example.tp4_almacenamientoexterno.entidades.Articulo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class BusquedaArticulo extends AsyncTask<String, Void, Articulo> {
+
+    private int id;
+
+    private Articulo articulo;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    protected Articulo doInBackground(String... strings) {
+        articulo = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(DataDB.urlMySQL, DataDB.user, DataDB.pass);
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM articulo WHERE id = " + id);
+
+            while(rs.next()) {
+                articulo = new Articulo();
+                articulo.setId(rs.getInt("id"));
+                articulo.setNombre(rs.getString("nombre"));
+                articulo.setStock(rs.getInt("stock"));
+                articulo.setIdCategoria(rs.getInt("idCategoria"));
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return articulo;
+    }
+}
